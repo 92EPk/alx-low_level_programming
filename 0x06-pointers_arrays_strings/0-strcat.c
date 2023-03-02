d176347765c9c773422ea6c354c0524e7eec9b44
Bx <53805165+92EPk@users.noreply.github.com>
2023-03-02 20:41:14 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
+ * _strcat - concat. str
+ * @dest: destn.
+ * @src: source
  *
- * Return: void
+ *
+ * Return: char dest
  */
+
 char *_strcat(char *dest, char *src)
+
 {
-	int i;
-	int j;
+	int i, j;
 
 	i = 0;
 	while (dest[i] != '\0')
@@ -24,3 +26,7 @@ char *_strcat(char *dest, char *src)
 		i++;
 		j++;
 	}
+
+	dest[i] = '\0';
+	return (dest);
+}
