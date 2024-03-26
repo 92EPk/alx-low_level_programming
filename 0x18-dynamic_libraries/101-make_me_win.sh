6377d555c90246c7fd99c3d7e3ee640a757ceb62
Bx <53805165+92EPk@users.noreply.github.com>
2024-03-26 22:12:27 +0200

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 wget -P https://github.com/92EPk/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=libgaga.so
+export LD_PRELOAD=/$PWD/libgaga.so
